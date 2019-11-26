b49461b160634c5ad6f7ac7fb6a400bb35abdbb0
David Antognoli <david.antognoli@gmail.com>
2019-11-26 14:56:43 -0600

    gutted cinemachine :disappointed:

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,28 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCharacterController : MonoBehaviour
 {
     [SerializeField]
+    private bool useCinemachineForCameraRotation = false;
+    [SerializeField]
     private Transform mainCameraTransform;
+    [SerializeField]
+    private float turnSpeed = 1.0f;
     [Tooltip("How much input before the character starts moving")]
     [SerializeField]
     private float movementInputThreshold = 0.1f;
     private Animator animator;
-    private float yInput;
+    private float yInput, xInput;
     private int sprintAnimParameter = Animator.StringToHash("IsSprinting");
     private int forwardAnimParameter = Animator.StringToHash("Forward");
+    private Vector3 cameraOffset;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        cameraOffset = mainCameraTransform.position - transform.position;
+
     }
     private void Update()
     {
         GetMovementInput();
         UpdateForwardAnimParameter();
+        if (!useCinemachineForCameraRotation)
+            UpdateCamera();
         UpdateRotation();
         UpdateSprintAnimParameter();
+    }
+
+    private void UpdateCamera()
+    {
+        mainCameraTransform.transform.position = transform.position + cameraOffset;
+        mainCameraTransform.Rotate(0, turnSpeed * xInput * Time.deltaTime, 0);
     }
 
     private void UpdateSprintAnimParameter()
@@ -36,7 +52,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
         if (Mathf.Abs(yInput) > movementInputThreshold)
         {
             var newRotation = transform.eulerAngles;
-            newRotation.y = mainCameraTransform.eulerAngles.y;
+            newRotation.y = mainCameraTransform.localEulerAngles.y;
             transform.eulerAngles = newRotation;
         }
     }
@@ -48,6 +64,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
     private void GetMovementInput()
     {
+        xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
     }
 
